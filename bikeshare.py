02490d51e4844e80465ace8f2db8f7be8414497a
chioma-grace <chigrace95@gmail.com>
2022-01-08 17:12:21 +0100

    style: format code

--- bikeshare.py
+++ bikeshare.py
@@ -3,9 +3,7 @@ import pandas as pd
 import numpy as np
 import json
 
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+CITY_DATA = { 'chicago': 'chicago.csv', 'new york city': 'new_york_city.csv', 'washington': 'washington.csv' }
 
 MONTHS = ['january', 'feburary', 'march', 'april', 'may', 'june']
 
